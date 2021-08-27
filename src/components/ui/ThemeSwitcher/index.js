36dde2911a17cbf65b7f2a2126eedf701999f4a5
MaksymAfanasiev <afanasiev.maksym@pdffiller.team>
2021-08-28 00:57:08 +0300

    fixes

--- src/components/ui/ThemeSwitcher/index.js
+++ src/components/ui/ThemeSwitcher/index.js
@@ -2,13 +2,12 @@ import { useState } from "react";
 import cn from "classnames";
 import Row from "../Row";
 
-import whiteModIcon from "../../../assets/svg/white-mod-icon.svg";
 import blackModIcon from "../../../assets/svg/black-mod-icon.svg";
 
 import css from "./index.module.scss";
 
 const ThemeSwitcher = () => {
-  const [whiteTheme, setWhiteTheme] = useState(false);
+  const [whiteTheme, setWhiteTheme] = useState(true);
 
   const handleChangeTheme = () => {
     setWhiteTheme((prev) => !prev);
@@ -17,12 +16,8 @@ const ThemeSwitcher = () => {
 
   return (
     <Row mod={css.themeSwitcher} onClick={handleChangeTheme}>
-      {whiteTheme ? (
-        <img alt="icon" src={blackModIcon} />
-      ) : (
-        <img alt="icon" src={whiteModIcon} />
-      )}
-      <p className={css.text}>{whiteTheme ? "White mode" : "Black mode"}</p>
+      <img alt="icon" src={blackModIcon} />
+      <p className={css.text}>Black mode</p>
       <div
         className={cn([
           css.switcherBtn,
